14e5b70d766cea3ad4d20abf80a9237afc185098
aditya0124 <139566319+aditya0124@users.noreply.github.com>
2024-05-12 11:00:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,7 @@ const todo = document.querySelector("#to-do-Box")
 
 items.addEventListener("keyup", (e) => {
   // console.log(e)
-  //console.log(e.key)//jo key press hoga wah mil jayega
   if(e.key ==="Enter") {
-    //1. console.log(items.value) work with both arrow and normal fn
-    
-    //2. console.log(this.value) not work with arrow fn, it search this near it, not lexically scoped
-    
-    //3. console.log(e.target.value)//printthe content inside box
     addtodo(e.target.value)
     e.target.value=""
   }
@@ -26,7 +20,7 @@ const addtodo = (item) => {
  //append child of to do
 
  listitem.addEventListener("click", function(){
-   (this.classList.toggle("done"))//this refers to li which we created up AND we toogle done or not done if our task done click on items it mark done so we add done in his classlist and vice-vera
+   (this.classList.toggle("done"))
  })
 listitem.querySelector("i").addEventListener("click", function(){
   listitem.remove()
